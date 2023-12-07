571940400023f45a84a184674a67ce4b8a63d125
Richard <richardramirez91@gmail.com>
2023-12-06 19:44:09 -0500

    Borrar finalizado

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
 
 import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
-import { switchMap } from 'rxjs';
+import { filter, switchMap, tap } from 'rxjs';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-new-page',
@@ -34,7 +37,8 @@ export class NewPageComponent implements OnInit {
     private heroesService: HeroesService,
     private ActivatedRoute: ActivatedRoute,
     private router: Router,
-    private snackbar: MatSnackBar,) {}
+    private snackbar: MatSnackBar,
+    private dialog: MatDialog) {}
 
   get currentHero(): Hero {
     const hero = this.heroForm.value as Hero;
@@ -83,6 +87,37 @@ export class NewPageComponent implements OnInit {
       this,this.router.navigate(['/heroes/edit', hero.id]);
       this.showSnackBar(`${hero.superhero} created`);
     });
+
+  }
+
+  onDeleteHero(){
+    if (!this.currentHero.id) throw Error('Hero id is required');
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: this.heroForm.value
+    });
+
+    dialogRef.afterClosed()
+    .pipe(
+      filter( (result: boolean) => result ),
+      switchMap( () => this.heroesService.deleteHeroById(this.currentHero.id) ),
+      filter( (wasDeleted: boolean) => wasDeleted),
+    )
+    .subscribe(() => {
+      this.router.navigate(['/heroes']);
+    });
+
+
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //   if (!result) return;
+
+    //   this.heroesService.deleteHeroById(this.currentHero.id)
+    //   .subscribe( wasDeleted => {
+    //     if (wasDeleted)
+    //        this.router.navigate(['/heroes']);
+    //   })
+    // });
 
   }
 
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -48,10 +48,11 @@ export class HeroesService {
   }
 
   deleteHeroById(id:string): Observable<boolean> {
+
     return this.http.delete(`${ this.baseUrl }/heroes/${id}`)
     .pipe(
+      map( resp => true),
       catchError( err => of(false)),
-      map( resp => true)
     );
   }
 
